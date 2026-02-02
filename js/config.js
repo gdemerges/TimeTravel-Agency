3449796d1a04459eed7d467db4c831d5cb8300cd
Guillaume Demergès <guillaume.demerges@protonmail.com>
2026-02-02 14:44:59 +0100

    chore: Remove config.js from tracking and fix deploy workflow

--- js/config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Configuration - TimeTravel Agency
- * Ce fichier contient vos clés API et n'est PAS versionné sur GitHub
- */
-
-const CONFIG = {
-    GEMINI_API_KEY: 'AIzaSyBz8p5tU4SvVwxqUn-iy_G9APFqF6xJlRE'
-};
